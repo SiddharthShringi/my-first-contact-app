15e3c70566917814e3d866b21381d624534167d6
BlackSamsung <siddharthshringi@gmail.com>
2018-06-11 16:59:50 +0530

    fix the following issues: 1. map url to the views 2. fix routing issue

---
+++ contact/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+app_name = 'contact'
+
+urlpatterns = [
+    path('', views.contact_list, name='contact_list'),
+    path('person<int:pk>', views.contact_detail, name='contact_detail')
+]
--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Person
 
-# Create your views here.
+
+def contact_list(request):
+    persons = Person.objects.all().order_by('first_name')
+    return render(request, 'contact/contact_list.html', {'persons': persons, 'count': persons.count()})
+
+
+def contact_detail(request, pk):
+    person = get_object_or_404(Person, pk=pk)
+    return render(request, 'contact/contact_detail.html', {'person': person})
